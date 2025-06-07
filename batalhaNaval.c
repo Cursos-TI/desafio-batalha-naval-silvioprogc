c224adb478c19c122cb954ff55e4b4027c8a0f10
silvioprogc <silviosramos@hotmail.com>
2025-06-07 20:39:34 +0000

    desafio batalha naval aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,87 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+#define TAM 10
+#define TAMANHO_NAVIO 3
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    char linha[10] = {'A','B','C','D','E','F','G','H','I','J'};
+    int tabuleiro[TAM][TAM];
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    // Inicializa o tabuleiro com
+    for (int i = 0; i < TAM; i++) {
+        for (int j = 0; j < TAM; j++) {
+            tabuleiro[i][j] = 0;
+        }
+    }
+    // Função auxiliar para verificar se pode posicionar o navio
+    int podePosicionar(int tab[TAM][TAM], int l, int c, int dir, int diag) {
+        for (int i = 0; i < TAMANHO_NAVIO; i++) {
+            int li = l + (diag ? i : (dir == 1 ? i : 0));
+            int co = c + (diag ? i : (dir == 0 ? i : 0));
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+            if (li >= TAM || co >= TAM || tab[li][co] != 0) return 0;
+        }
+        return 1;
+    }
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+   void posicionar(int tab[TAM][TAM], int l, int c, int dir, int diag) {
+        for (int i = 0; i < TAMANHO_NAVIO; i++) {
+            int li = l + (diag ? i : (dir == 1 ? i : 0));
+            int co = c + (diag ? i : (dir == 0 ? i : 0));
+            tab[li][co] = 3;
+        }
+    }
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    // --- Navios ---
+
+    // Navio horizontal
+    int lh1 = 2, ch1 = 4;
+    if (podePosicionar(tabuleiro, lh1, ch1, 0, 0))
+        posicionar(tabuleiro, lh1, ch1, 0, 0);
+
+    // Navio vertical
+    int lv1 = 5, cv1 = 2;
+    if (podePosicionar(tabuleiro, lv1, cv1, 1, 0))
+        posicionar(tabuleiro, lv1, cv1, 1, 0);
+
+    // Navio diagonal principal
+    int ld1 = 0, cd1 = 1;
+    if (podePosicionar(tabuleiro, ld1, cd1, 0, 1))
+        posicionar(tabuleiro, ld1, cd1, 0, 1);
+
+    // Navio diagonal 
+    int ld2 = 6, cd2 = 9;
+    int pode = 1;
+    for (int i = 0; i < TAMANHO_NAVIO; i++) {
+        int li = ld2 + i;
+        int co = cd2 - i;
+        if (li >= TAM || co < 0 || tabuleiro[li][co] != 0) {
+            pode = 0;
+            break;
+        }
+    }
+    if (pode) {
+        for (int i = 0; i < TAMANHO_NAVIO; i++) {
+            int li = ld2 + i;
+            int co = cd2 - i;
+            tabuleiro[li][co] = 3;
+        }
+    }
+
+    // Exibir o tabuleiro
+    printf("   ");
+    for (int j = 0; j < TAM; j++) {
+        printf("%d ", j);
+    }
+    printf("\n");
+
+    for (int i = 0; i < TAM; i++) {
+        printf("%c: ", linha[i]);
+        for (int j = 0; j < TAM; j++) {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
